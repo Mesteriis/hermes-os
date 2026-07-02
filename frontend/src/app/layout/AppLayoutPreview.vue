7e2569798cabcb85f4b66722a5cc1d1f1efbba10
Aleksandr Meshchriakov <avm@sh-inc.ru>
2026-07-02 16:54:18 +0200

    Add app layout preview scaffold

---
+++ frontend/src/app/layout/AppLayoutPreview.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppLayout from './AppLayout.vue'
+</script>
+
+<template>
+	<section class="app-layout-preview">
+		<AppLayout />
+	</section>
+</template>
---
+++ frontend/src/app/layout/app-layout-preview.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import AppLayoutPreview from './AppLayoutPreview.vue'
+import '../../shared/ui/styles/index.css'
+import './app-layout.css'
+
+createApp(AppLayoutPreview).mount('#app-layout-preview')
