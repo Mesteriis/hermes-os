9aeddea605892f53596f8ede20da08e56fa383c4
Aleksandr Meshchriakov <avm@sh-inc.ru>
2026-07-20 16:10:09 +0200

    fix(development): include registration descriptor module

--- backend/development/runtime/src/modules/registration/mod.rs
+++ backend/development/runtime/src/modules/registration/mod.rs
@@ -1,2 +1,4 @@
+#[path = "../../../../../src/kernel/src/modules/registration/descriptor/mod.rs"]
+pub(crate) mod descriptor;
 #[path = "../../../../../src/kernel/src/modules/registration/registry.rs"]
 pub(crate) mod registry;
